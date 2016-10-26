14692785500836ee7adf8fdd2ebed20e6bfd0a0a
Gelson Moraes <analyst.gelson@gmail.com>
2016-10-26 08:27:05 -0300

    new

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 var app = new Vue({
 	el: "#app",
 	data: {
+		test: '',
 		title: "Conta a pagar",
 		menus: [
 			{id: 0, name: "Listar conta"}, 
 			{id: 1, name: "Criar conta"}
 		],
-		activedView: 0,
+		activedView: 1,
+		formType: 'insert',
+		bill: {
+			date_due: '',
+			name: '',
+			value: 0,
+			done: 0
+		},
+		names: [
+			'Conta de luz',
+			'Conta de água',
+			'Conta de telefone',
+			'Supermercado',
+			'Empréstimo',
+			'Gasolina'
+		],
 		bills: [
 			{date_due: '20/08/2016', name: 'Conta de luz', value: 70.99, done: 1},
 			{date_due: '21/08/2016', name: 'Conta de água', value: 55.99, done: 0},
@@ -31,6 +47,43 @@ var app = new Vue({
 	methods: {
 		showView: function(id){
 			this.activedView=id;
+			if (id == 1) {
+				this.formType = 'insert';
+			}
+		},
+		submit: function(){
+			if (this.formType == 'insert') {
+				this.bills.push(this.bill);
+			}
+			this.bill = {
+				date_due: '',
+				name: '',
+				value: 0,
+				done: 0
+			};
+
+			this.activedView = 0;
+		},
+		loadBill: function(bill){
+			this.bill = bill;
+			this.activedView = 1;
+			this.formType = 'update';
 		}	
 	}
-})
+});
+
+Vue.filter('doneLabel', function(value){
+	if (value == 0) {
+		return "Não paga";
+	}else {
+		return "Paga";
+	}
+});
+
+
+
+
+
+
+
+
